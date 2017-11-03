7ef961f601294bb390691d75817c3c51fc4934fd
Adesun <wowgreatocr@gmail.com>
2017-11-03 23:41:01 +0800

    fix typo

--- Modernizr.2.6.2/Tools/uninstall.ps1
+++ Modernizr.2.6.2/Tools/uninstall.ps1
@@ -2,5 +2,4 @@ param($installPath, $toolsPath, $package, $project)
 
 . (Join-Path $toolsPath common.ps1)
 
-# Update the _references.js file
 Remove-Reference $scriptsFolderProjectItem $modernizrFileNameRegEx
